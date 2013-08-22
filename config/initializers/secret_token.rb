c39064cec8bf304f334132527db172e39e51ff2a
Your Name <jsroldan@zoho.com>
2013-08-21 19:38:58 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '55fb14103deb51031510486504aa38753d5be3f37a68321fde661ccd9e64f5c1a56e97309d54cd836525cc45b25a4ee47dfb970e91b66f35251831ea4d549a3a'
